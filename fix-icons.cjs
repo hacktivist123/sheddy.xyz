43ac9ef0d0b87d33f5dba775efb2b199d2e2c88a
hacktivist123 <akintayoshedrack@gmail.com>
2026-02-05 11:49:29 +0000

    chore: remove debug script

--- fix-icons.cjs
+++
@@ -1,8 +0,0 @@
-const icons = require("simple-icons");
-const keys = Object.keys(icons);
-const broken = ["terraform", "javascript", "kubernetes", "prometheus"];
-
-broken.forEach((term) => {
-  const matches = keys.filter((k) => k.toLowerCase().includes(term));
-  console.log(`${term}:`, matches);
-});
